1f396632dde597778a315e428c14ce7a7b359acd
EC2 Default User <ec2-user@ip-172-31-13-69.ap-south-1.compute.internal>
2021-04-17 11:55:32 +0530

    api gateway docs

--- src/main/java/com/teamteach/journalmgmt/infra/api/IJournalResource.java
+++ src/main/java/com/teamteach/journalmgmt/infra/api/IJournalResource.java
@@ -5,57 +5,65 @@ import com.teamteach.journalmgmt.domain.models.*;
 import com.teamteach.journalmgmt.domain.usecases.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
 
 @RequestMapping("journals")
 public interface IJournalResource {
 
+    @ApiIgnore
     @PostMapping("/create")
         ResponseEntity<ObjectResponseDto> saveJournal(@RequestBody @Valid JournalCommand journalCommand);
 
-    // @GetMapping("/")
-    //     ResponseEntity<ObjectListResponseDto<JournalResponse>> findAllJournals();
-
-    @GetMapping("/parent/{ownerId}")
+    @GetMapping("/owner/{ownerId}")
         ResponseEntity<ObjectListResponseDto<JournalResponse>> findJournalById(@PathVariable String ownerId) ;
     
-    // @GetMapping("/title/{title}")
-    //     ResponseEntity<ObjectResponseDto> findJournalByTitle(@PathVariable String title) ;
-
+    @ApiIgnore
     @DeleteMapping("/{id}")
         ResponseEntity<ObjectResponseDto> deleteJournal(@PathVariable String id) ;
 
+    @ApiIgnore
     @PostMapping("/moods")
         ResponseEntity<ObjectResponseDto> saveMood(@RequestBody Mood mood);
 
+    @ApiIgnore
     @GetMapping("/moods")
         ResponseEntity<ObjectListResponseDto> findAllMoods();
 
+    @ApiIgnore
     @GetMapping("/moods/id/{id}")
         ResponseEntity<ObjectResponseDto> findMoodById(@PathVariable("id") String id);
 
+    @ApiIgnore
     @GetMapping("/moods/name/{name}")
         ResponseEntity<ObjectResponseDto> findMoodByName(@PathVariable("name") String name);
 
+    @ApiIgnore
     @DeleteMapping("/moods/{id}")
         ResponseEntity<ObjectResponseDto> deleteMood(@PathVariable("id") String id);
 
+    @ApiIgnore
     @PostMapping("/categories")
         ResponseEntity<ObjectResponseDto> saveCategory(@RequestBody Category category);
 
+    @ApiIgnore
     @GetMapping("/categories")
         ResponseEntity<ObjectListResponseDto> findCategories();
 
+    @ApiIgnore
     @GetMapping("/categories/id/{id}")
         ResponseEntity<ObjectResponseDto> findCategoryById(@PathVariable("id") String id);
 
+    @ApiIgnore
     @GetMapping("/categories/title/{title}")
         ResponseEntity<ObjectResponseDto> findCategoryByTitle(@PathVariable("title") String title);
 
+    @ApiIgnore
     @GetMapping("/categories/colour/{colour}")
         ResponseEntity<ObjectResponseDto> findCategoryByColour(@PathVariable("colour") String colour);
 
+    @ApiIgnore
     @DeleteMapping("/categories/{id}")
         ResponseEntity<ObjectResponseDto> deleteCategory(@PathVariable("id") String id);
 }
