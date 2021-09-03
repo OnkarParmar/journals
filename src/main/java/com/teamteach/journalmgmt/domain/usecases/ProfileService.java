f87d0f10d19a3a115a9ee2bbd0c4619feb882ff9
Digital Sherpa <digisherpa@OnkarMac.local>
2021-09-03 13:23:12 +0530

    message

--- src/main/java/com/teamteach/journalmgmt/domain/usecases/ProfileService.java
+++ src/main/java/com/teamteach/journalmgmt/domain/usecases/ProfileService.java
@@ -34,6 +34,7 @@ public class ProfileService {
             headers.set("Authorization", accessToken); 
             headers.setContentType(MediaType.APPLICATION_JSON);
             HttpEntity <String> entity = new HttpEntity <> (null, headers);
+            System.out.println(parentProfileUrl);
             ResponseEntity <String> response = restTemplate.exchange(parentProfileUrl, HttpMethod.GET, entity, String.class);
             JsonNode respoJsonNode = new ObjectMapper().readTree(response.getBody());
             boolean success = respoJsonNode.get("success").asBoolean();
